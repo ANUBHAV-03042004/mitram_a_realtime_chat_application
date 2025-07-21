8ef6afea95ba6d9cb81333e766b06203dc6f58dd
Ubuntu <ubuntu@ip-172-31-86-153.ec2.internal>
2025-07-21 17:20:32 +0000

    Reapply "update profile img"
    This reverts commit e3f2d8cd5e94003cb8893b8426e8e64599b0755b.

--- resources/views/vendor/Chatify/layouts/listItem.blade.php
+++ resources/views/vendor/Chatify/layouts/listItem.blade.php
@@ -7,7 +7,7 @@
             <div class="saved-messages avatar av-m">
                 <!-- <span class="far fa-bookmark"></span> -->
            <!-- <img src="{{ asset('storage/users-avatar/' . Auth::user()->avatar) }}" alt="User Avatar" style="width:40px; height:40px; border-radius:50%;" /> -->
-<img src="{{ asset('storage/users-avatar/' . (Auth::user()->avatar ?? 'avatar.png')) }}"
+<img src="{{ asset('storage/users-avatar/' . Auth::user()->avatar) }}"
      alt="User Avatar"
      style="width:40px; height:40px; border-radius:50%;" />
             </div>
